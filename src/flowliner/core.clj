bcb1feab9d2e05bbb1e773a6df184c2e55a078d7
Jukka Villstedt <jukka.villstedt@gmail.com>
2014-11-09 22:45:52 +0200

    x

--- src/flowliner/core.clj
+++ src/flowliner/core.clj
@@ -1,5 +1,6 @@
 (ns outliner.core
   (:require [clojure.core.async :as async]
+            [datomic.api :as d]
             (flow-gl.gui [drawable :as drawable]
                          [layout :as layout]
                          [layouts :as layouts]
@@ -15,7 +16,8 @@
             [flow-gl.debug :as debug])
   (:import [java.io File]
            [java.util.concurrent Executors]
-           [java.lang Runnable])
+           [java.lang Runnable]
+           [java.nio ByteBuffer])
   (:use flow-gl.utils
         clojure.test))
 
@@ -41,6 +43,7 @@
 
    :default
    [state true]))
+
 
 (gui/def-control text-editor
   ([view-context control-channel]
@@ -76,10 +79,13 @@
 
 (gui/def-control outliner
   ([view-context control-channel]
-     (conj {:nodes [{:text "foo"
-                     :children [{:text "bar"}
-                                {:text "baz"}]}]}
-           gui/child-focus-handlers))
+     (let [db-uri "datomic:mem://flowliner"]
+       (d/create-database db-uri)
+       (conj {:conn (d/connect db-uri)
+              :nodes [{:text "foo"
+                       :children [{:text "bar"}
+                                  {:text "baz"}]}]}
+             gui/child-focus-handlers)))
 
   ([view-context {:keys [nodes]}]
      (layouts/->VerticalStack
@@ -94,3 +100,115 @@
 
 (gui/redraw-last-started-view)
 
+
+
+(defn ids-to-bytes [ids]
+  (let [buffer (ByteBuffer/allocate (* Long/BYTES (count ids)))]
+    (doseq [id ids]
+      (.putLong buffer id))
+    (.array buffer)))
+
+(defn bytes-to-ids [bytes]
+  (let [buffer (ByteBuffer/allocate (count bytes))]
+    (doto buffer
+      (.put bytes)
+      (.flip))
+    (loop [ids []]
+      (if (> (.remaining buffer) 0)
+        (recur (conj ids (.getLong buffer)))
+        ids))))
+
+(defn attribute [ident value-type cardinality ]
+  {:db/id (d/tempid :db.part/db)
+   :db/ident ident
+   :db/valueType value-type
+   :db/cardinality cardinality
+   :db.install/_attribute :db.part/db})
+
+(defn set-text [node text]
+  [:db/add
+   node
+   :flowliner.node/text
+   text])
+
+(defn set-children [parent children]
+  [:db/add
+   parent
+   :flowliner.node/children
+   (ids-to-bytes children)])
+
+(defn get-children [parent-entity]
+  (if-let [bytes (:flowliner.node/children parent-entity)]
+    (bytes-to-ids bytes)
+    []))
+
+(defn create-node [text conn]
+  (let [node-tempid (d/tempid :db.part/user)
+        {:keys [tempids db-after]} @(d/transact
+                                     conn
+                                     [(set-text node-tempid text)])]
+    (d/resolve-tempid db-after tempids node-tempid)))
+
+(def add-child
+  (d/function '{:lang :clojure
+                :params [db parent child]
+                :requires [[outliner.core :as core]]
+                :code [(core/set-children parent (-> (d/entity db parent)
+                                                     (core/get-children)
+                                                     (conj child)))] }))
+
+(defn set-root [node]
+  [:db/add
+   node
+   :flowliner.node/root
+   true])
+
+(defn get-root [db]
+  (ffirst (d/q '[:find ?node
+                 :where [?node :flowliner.node/root true]]
+               db)))
+
+(let [db-uri "datomic:mem://flowliner"]
+  (d/create-database db-uri)
+  (let [conn (d/connect db-uri)]
+    (d/transact
+     conn
+     [(attribute :flowliner.node/text
+                 :db.type/string
+                 :db.cardinality/one)
+      (attribute :flowliner.node/children
+                 :db.type/bytes
+                 :db.cardinality/one)
+      (attribute :flowliner.node/root
+                 :db.type/boolean
+                 :db.cardinality/one)])
+
+    (d/transact
+     conn
+     [{:db/id (d/tempid :db.part/user)
+       :db/ident :flowliner/add-child
+       :db/fn add-child}])
+
+    (let [node-1 (d/tempid :db.part/user)
+          node-2 (d/tempid :db.part/user)
+          {:keys [tempids db-after]} @(d/transact
+                                       conn
+                                       [(set-root node-1)
+                                        (set-text node-1 "root")
+                                        (set-text node-2 "child")])
+          parent (d/resolve-tempid db-after tempids node-1)
+          child (d/resolve-tempid db-after tempids node-2)]
+
+      #_(d/invoke (d/db conn) :flowliner/add-child (d/db conn) parent child)
+      (d/transact
+       conn
+       [#_[:db/add parent :flowliner.node/children (ids-to-bytes [child])]
+        [:flowliner/add-child parent child]])
+
+      (let [db (d/db conn)]
+        (->> parent
+             (d/entity db)
+             (get-children)
+             (first)
+             (d/entity db)
+             :flowliner.node/text)))))
